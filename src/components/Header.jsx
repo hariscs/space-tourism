ebd344034d483f2b3267b33c1e7777fbf49d63ce
Haris Shah <harisshah385@gmail.com>
2022-01-23 21:41:45 +0500

    add home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './Header.module.css';
 
 const Header = () => {
@@ -10,9 +10,9 @@ const Header = () => {
 		<header className={styles.header}>
 			<div className={` ${styles.header__flex}`}>
 				<div className={styles.logo}>
-					<Link to='/'>
+					<NavLink to='/'>
 						<img src='/assets/shared/logo.svg' alt='logo' />
-					</Link>
+					</NavLink>
 				</div>
 				<nav className={`${styles.nav}`}>
 					<ul
@@ -23,32 +23,32 @@ const Header = () => {
 						}
 					>
 						<li className={styles.list__item}>
-							<Link
+							<NavLink
 								to='/home'
 								className={`${styles.page__active} ${styles.list__link}`}
 							>
 								<span>00</span>
 								home
-							</Link>
+							</NavLink>
 						</li>
 
 						<li className={styles.list__item}>
-							<Link to='/destinations' className={styles.list__link}>
+							<NavLink to='/destinations' className={styles.list__link}>
 								<span>01</span>
 								destinations
-							</Link>
+							</NavLink>
 						</li>
 						<li className={styles.list__item}>
-							<Link to='/about' className={styles.list__link}>
+							<NavLink to='/about' className={styles.list__link}>
 								<span>02</span>
 								crew
-							</Link>
+							</NavLink>
 						</li>
 						<li className={styles.list__item}>
-							<Link to='/technology' className={styles.list__link}>
+							<NavLink to='/technology' className={styles.list__link}>
 								<span>03</span>
 								technology
-							</Link>
+							</NavLink>
 						</li>
 					</ul>
 				</nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,25 @@ const Home = () => {
 	return (
 		<div className={styles.home}>
 			<Header />
+			<main className={` grid__container ${styles.main} `}>
+				<div className={styles.main__info}>
+					<h1 className={styles.main__title}>
+						so, you want to travel to
+						<span> space</span>
+					</h1>
+					<p className={styles.main__subtitle}>
+						Let’s face it; if you want to go to space, you might as well
+						genuinely go to outer space and not hover kind of on the edge of it.
+						Well sit back, and relax because we’ll give you a truly out of this
+						world experience!
+					</p>
+				</div>
+				<div className={styles.main__button}>
+					<a href='/' className={styles.btn}>
+						explore
+					</a>
+				</div>
+			</main>
 		</div>
 	);
 };
