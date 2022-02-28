210a2a329688ee821a9592a29ac4cb667855ef6f
Haris Shah <harisshah385@gmail.com>
2022-02-28 22:25:56 +0500

    add tab component

--- src/components/DestTabs.jsx
+++ src/components/DestTabs.jsx
@@ -1,18 +1,60 @@
+import { useState } from 'react';
+import styles from '../styles/components/DestTabs.module.css';
+
+const TabsData = [
+	{
+		id: 'Tab 1',
+		title: 'Tab 1',
+		body: 'Lorem 1 ipsum dolor sit amet, consectetur adipisicing elit.',
+		img: 'img 1 here',
+	},
+	{
+		id: 'Tab 2',
+		title: 'Tab 2',
+		body: 'Lorem 2 ipsum dolor sit amet, consectetur adipisicing elit.',
+		img: 'img 2 here',
+	},
+	{
+		id: 'Tab 3',
+		title: 'Tab 3',
+		body: 'Lorem 3 ipsum dolor sit amet, consectetur adipisicing elit.',
+		img: 'img 3 here',
+	},
+];
+
 const DestTabs = () => {
-	const handler = (e) => {
-		console.log(e.target.getAttribute('data-index'));
+	const [activeTab, setActiveTab] = useState(TabsData[0].id);
+	const handleClick = (newActiveTab) => {
+		setActiveTab(newActiveTab);
 	};
 	return (
-		<div className='grid__container'>
-			<ul>
-				{/*	<li data-index={index} onClick={handler}>
-					<h2 style={{ color: 'white' }}>Dest tabs</h2>
-				</li>
-				 <li>
-					<h2>Dest tabs</h2>
-				</li> */}
-			</ul>
-		</div>
+		<main className={styles.tab}>
+			<div className={styles.tab__btn}>
+				{TabsData.map((data) => (
+					<button
+						key={data.id}
+						onClick={() => handleClick(data.id)}
+						className={data.id === activeTab ? styles.current : null}
+					>
+						{data.title}
+					</button>
+				))}
+			</div>
+			{TabsData.map(
+				(data) =>
+					data.id === activeTab && (
+						<div key={data.id} className={styles.tab__flex}>
+							<div className={styles.tab__img}>
+								<h1>{data.img}</h1>
+							</div>
+							<div className={styles.tab__info}>
+								<h2>{data.title}</h2>
+								<p>{data.body}</p>
+							</div>
+						</div>
+					)
+			)}
+		</main>
 	);
 };
 
