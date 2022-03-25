473e3b1c04c6e68e69beea950095a7c5ba39d39f
Haris Shah <harisshah385@gmail.com>
2022-03-25 16:45:37 +0500

    add the destination tabs

--- src/container/DestTabs/DestTabs.js
+++ src/container/DestTabs/DestTabs.js
@@ -9,31 +9,38 @@ const DestTabs = () => {
 	};
 	return (
 		<main className={styles.tab}>
-			{data.destinations.map((data) => (
-				<button
-					key={data.id}
-					onClick={() => handleClick(data.id)}
-					className={data.id === activeTab ? styles.current : null}
-				>
-					{data.name}
-				</button>
-			))}
-			{data.destinations.map(
-				(data) =>
-					data.id === activeTab && (
-						<div key={data.id}>
-							<div className={styles.tab__info}>
+			<div className={styles.tab__img}>
+				{data.destinations.map(
+					(data) =>
+						data.id === activeTab && <img src={data.image} alt={data.name} />
+				)}
+			</div>
+			<div className={styles.tab__info}>
+				<div className={styles.tab__infoBtns}>
+					{data.destinations.map((data) => (
+						<button
+							key={data.id}
+							onClick={() => handleClick(data.id)}
+							className={data.id === activeTab ? styles.current : null}
+						>
+							{data.name}
+						</button>
+					))}
+				</div>
+				<div className={styles.tab__infoDetails}>
+					{data.destinations.map(
+						(data) =>
+							data.id === activeTab && (
 								<div className={styles.tab__info}>
-									<h2>{data.name}</h2>
-									<p>{data.description}</p>
+									<div className={styles.tab__info}>
+										<h2>{data.name}</h2>
+										<p>{data.description}</p>
+									</div>
 								</div>
-							</div>
-							<div className={styles.tab__img}>
-								<img src={data.image} alt={data.name} />
-							</div>
-						</div>
-					)
-			)}
+							)
+					)}
+				</div>
+			</div>
 		</main>
 	);
 };
